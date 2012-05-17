e4a9df9466b9258043f653700c06085567c385a9
Rafael Fernández López <ereslibre@ereslibre.es>
2012-05-17 03:56:07 +0200

    Fix typos

--- lib/generators/inferred_slug/install/templates/slug.rb
+++ lib/generators/inferred_slug/install/templates/slug.rb
@@ -1,10 +1,10 @@
 #
 # This generator automatically adds slugs to all your models. If you want to use
 # inferred slugs on individual models, delete this file and make your desired
-# models include the 'Slug' module. This is:
+# models include the 'InferredSlug::Slug' module. This is:
 #
 #   class MyModel < ActiveRecord::Base
-#     include Slug
+#     include InferredSlug::Slug
 #   end
 #
 class ActiveRecord::Base
